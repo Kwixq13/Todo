ef9113082d78a0a807f8f61bc771b4e43e99ea82
Salamandra19977 <salamandra199720@gmail.com>
2026-01-22 20:57:20 +0200

    last

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import style from "./Main.module.scss"
+
+function Main(props) {
+  return (
+    <div className={style.wrapper}>Main</div>
+  )
+}
+
+Main.propTypes = {}
+
+export default Main
+
